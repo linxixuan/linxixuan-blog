f512238010617a0117c480dc29c1fd66ac733417
linxixuan <zmx.morgan@gmail.com>
2014-08-14 00:21:38 +0800

    增加图库功能

--- routes/index.js
+++ routes/index.js
@@ -208,4 +208,48 @@ module.exports = function(app){
             }
         });
     });
+
+    // 图库
+    app.get('/pic', function (req, res) {
+        var data = commonData,
+            galleryPath = 'public/gallery',
+            foldPaths,
+            picPaths,
+            firstPic,
+            foldList = [],
+            picList = [];
+        if (!req.query.fold) {
+            foldPaths = fs.readdirSync(galleryPath);
+            for (var i = 0,len = foldPaths.length; i < len; i++) {
+                if (foldPaths[i] !== '.DS_Store') {
+                    if (fs.readdirSync(galleryPath + '/' + foldPaths[i])[0] === '.DS_Store') {
+                        firstPic = fs.readdirSync(galleryPath + '/' + foldPaths[i])[1];
+                    } else {
+                        firstPic = fs.readdirSync(galleryPath + '/' + foldPaths[i])[0];
+                    }
+                    firstPic = 'gallery/' +  foldPaths[i] + '/' + firstPic;
+                    foldList.push({
+                        'name': foldPaths[i],
+                        'firstPic': firstPic
+                    });
+                }
+            }
+        } else {
+            var fold = req.query.fold;
+            picPaths = fs.readdirSync(galleryPath + '/' + fold);
+            for (var i = 0,len = picPaths.length; i < len; i++) {
+                if (picPaths[i] !== '.DS_Store') {
+                    picList.push({
+                        'name': picPaths[i].replace(/\.\S+/g, ''),
+                        'imgSrc': 'gallery/' + fold + '/' + picPaths[i]
+                    })
+                }
+            }
+        }
+
+        // 渲染数据填充
+        data.foldList = foldList;
+        data.picList = picList;
+        res.render('pic', data);
+    });
 };
