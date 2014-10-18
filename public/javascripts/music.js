7e6f1872e27ba7bf3dae42f4b4ebe06f31f53bc2
linxixuan <zmx.morgan@gmail.com>
2014-10-19 01:05:53 +0800

    add random play feature

--- public/javascripts/music.js
+++ public/javascripts/music.js
@@ -1,3 +1,4 @@
+var player;
 $(function () {
     init();
 
@@ -13,6 +14,7 @@ $(function () {
     }
 
     function bindControll() {
+        player = $('.J-placeholder iframe')[0].contentWindow.document.querySelector('audio');
         $('body').delegate('.music-info .cover, .music-info .J-status', 'click', function () {
             if (!player) return;
 
@@ -27,7 +29,10 @@ $(function () {
                 $('.music-info .status-wrapper').removeClass('status-wrapper--anim');
             });
         });
+        bindAudio(player);
+    }
 
+    function bindAudio(player) {
         player.onplay = function () {
             $('.music-info .J-status').addClass('fa-pause');
             $('.music-info .J-status').removeClass('fa-play');
@@ -38,6 +43,14 @@ $(function () {
             $('.music-info .J-status').removeClass('fa-pause');
             $('.music-info .J-status').addClass('fa-play');
             $('.music-info .J-status').addClass('status--pause');
+        }
+
+        player.onended = function () {
+            var index = Math.floor(Math.random() * 10);
+            changeSong(index);
+            $('.music-info .J-status').addClass('fa-pause');
+            $('.music-info .J-status').removeClass('fa-play');
+            $('.music-info .J-status').removeClass('status--pause');
         }
     }
 
@@ -54,35 +67,45 @@ $(function () {
             $(this).addClass('active');
         });
         $('.music-list li').click(function () {
-            var id = $(this).find('.music__title').data('id'),
-                title = $(this).find('.music__title').data('title'),
-                url = $(this).find('.music__url').html();
-            $.ajax({
-                url: '/getMusicInfo?id=' + id,
-                method: 'get'
-            }).done(function(data) {
-                var music = JSON.parse(data),
-                    ndInfo = $('.music-info'),
-                    author = [];
-                
-                ndInfo.find('.cover')[0].src = music.image.replace(/spic/, 'lpic');
-                ndInfo.find('.name')[0].innerHTML = title;
-                ndInfo.find('.track')[0].innerHTML = music.alt_title;
-                for(var p in music.author) {
-                    author.push(music.author[p].name); 
-                }
-                author = author.join('/');
-                ndInfo.find('.author')[0].innerHTML = author;
-                ndInfo.find('.description')[0].innerHTML = music.summary;
+            var index = $('.music-list li').index(this);
+            changeSong(index);
+        });
+    }
 
-                player.src = url;
-            });
+    function changeSong(index) {
+        var ndTarget = $($('.music-list li')[index]),
+            id = ndTarget.find('.music__title').data('id'),
+            title = ndTarget.find('.music__title').data('title'),
+            url = ndTarget.find('.music__url').html();
+        $.ajax({
+            url: '/getMusicInfo?id=' + id,
+            method: 'get'
+        }).done(function(data) {
+            var music = JSON.parse(data),
+                ndInfo = $('.music-info'),
+                author = [];
+            
+            ndInfo.find('.cover')[0].src = music.image.replace(/spic/, 'lpic');
+            ndInfo.find('.name')[0].innerHTML = title;
+            ndInfo.find('.track')[0].innerHTML = music.alt_title;
+            for(var p in music.author) {
+                author.push(music.author[p].name); 
+            }
+            author = author.join('/');
+            ndInfo.find('.author')[0].innerHTML = author;
+            ndInfo.find('.description')[0].innerHTML = music.summary;
+
+            player.src = url;
+            setAudio($($('.music-list li')[index]).find('.music__url').html());
         });
     }
 
     // 设置音乐插件
     function setAudio(url) {
-        $('.J-placeholder').html(ReferrerKiller.htmlString('<audio controls autoplay name="media"><source src="' + url + '" type="audio/mpeg"></audio>'));
+        $('.J-placeholder').html(ReferrerKiller.htmlString('<audio controls name="media"><source src="' + url + '" type="audio/mpeg"></audio>'));
+        player = $('.J-placeholder iframe')[0].contentWindow.document.querySelector('audio');
+        player.play();
+        bindAudio(player);
     }
 
     // 插入控制功能
@@ -90,44 +113,6 @@ $(function () {
         var innerWindow = $('.J-placeholder').find('iframe')[0].contentWindow,
             ndBody = $(innerWindow.document.body);
         ndBody.append('<script src="' + location.origin + '/javascripts/jquery-1.11.1.min.js"></script>');
-        player = ndBody.find('audio')[0];
+        player = ndBody[0].querySelector('audio');
     }
-
-    /**
-     * 音乐控制对象
-     */
-    controller = {
-
-        // 当前播放音乐的ID
-        currentID: null,
-
-        // 播放器
-        player: null,
-
-        // 自动播放
-        autoPlay: true,
-
-        /**
-         * 初始化
-         * @param node control selector
-         */
-        init: function () {
-            this.player = $();
-        },
-
-        // 暂停
-        pause: function () {
-        },
-        
-        /**
-         * 播放
-         */
-        play: function () {
-        },
-
-        // 下一首
-        next: function () {
-        }
-        //
-    };
 });
