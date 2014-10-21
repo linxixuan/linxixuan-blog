425e8a6d7f1ccb1e04db38720ccb1c267e1368d4
linxixuan <zmx.morgan@gmail.com>
2014-10-21 20:12:41 +0800

    fix

--- public/javascripts/tool.js
+++ public/javascripts/tool.js
@@ -1,6 +1,6 @@
 (function () {
     $('body').append('<i class="J-up-top up-top fa fa-chevron-up"></i>');
-    $('body').delegate('click', '.J-up-top', function () {
+    $('.J-up-top').on('click', function () {
         window.scrollTo(0, 0);
     });
 })();
