c498bca189f26eeaf3dc9ed444fbe100da4277c2
linxixuan <zmx.morgan@gmail.com>
2014-10-21 20:09:48 +0800

    add requirejs

--- public/javascripts/base.js
+++ public/javascripts/base.js
@@ -1,21 +1,19 @@
-$(function () {
-    window.onload = function () {
-        $('.pg-index header').addClass('header--anim');
+define(['jquery', 'bootstrap', 'ga', 'tool'], function ($) {
+    $('.pg-index header').addClass('header--anim');
 
-        $('header .nav li').each(function (index) {
-            $(this).addClass('li--anim');
-            $(this).css('transition-delay', (0.5 + index * 0.2) + 's');
-            $(this).css('-webkit-transition-delay', (0.5 + index * 0.2) + 's');
-        });
+    $('header .nav li').each(function (index) {
+        $(this).addClass('li--anim');
+        $(this).css('transition-delay', (0.5 + index * 0.2) + 's');
+        $(this).css('-webkit-transition-delay', (0.5 + index * 0.2) + 's');
+    });
 
-        $('header .social').addClass('social--anim');
+    $('header .social').addClass('social--anim');
 
-        $('.pg-index .blog').each(function (index) {
-            $(this).addClass('blog--anim');
-            $(this).css('transition-delay', (1.5 + index * 0.2) + 's');
-            $(this).css('-webkit-transition-delay', (1.5 + index * 0.2) + 's');
-        });
+    $('.pg-index .blog').each(function (index) {
+        $(this).addClass('blog--anim');
+        $(this).css('transition-delay', (1.5 + index * 0.2) + 's');
+        $(this).css('-webkit-transition-delay', (1.5 + index * 0.2) + 's');
+    });
 
-        $('.pg-index .axis').addClass('axis--anim');
-    };
+    $('.pg-index .axis').addClass('axis--anim');
 });
--- public/javascripts/music.js
+++ public/javascripts/music.js
@@ -1,121 +1,124 @@
-var player;
-$(function () {
-    init();
+define(["jquery", "referrerkiller", "tool", "ga"], function ($) {
+    var player;
+    $(function () {
+        init();
 
-    function init() {
-        setAudio($('.music-list li .music__url').html());
-        insertControllers();
-        bindEvent();
-    }
+        function init() {
+            setAudio($('.music-list li .music__url').html());
+            insertControllers();
+            bindEvent();
+        }
 
-    function bindEvent() {
-        bindPlayList();
-        bindControll();
-    }
+        function bindEvent() {
+            bindPlayList();
+            bindControll();
+        }
 
-    function bindControll() {
-        player = $('.J-placeholder iframe')[0].contentWindow.document.querySelector('audio');
-        $('body').delegate('.music-info .cover, .music-info .J-status', 'click', function () {
-            if (!player) return;
+        function bindControll() {
+            player = $('.J-placeholder iframe')[0].contentWindow.document.querySelector('audio');
+            $('body').delegate('.music-info .cover, .music-info .J-status', 'click', function () {
+                if (!player) return;
 
-            if(player.paused) {
-                player.play();
-            } else {
-                player.pause();
-            }
+                if(player.paused) {
+                    player.play();
+                } else {
+                    player.pause();
+                }
 
-            $('.music-info .status-wrapper').addClass('status-wrapper--anim');
-            $('.music-info .status-wrapper')[0].addEventListener('webkitAnimationEnd', function () {
-                $('.music-info .status-wrapper').removeClass('status-wrapper--anim');
+                $('.music-info .status-wrapper').addClass('status-wrapper--anim');
+                $('.music-info .status-wrapper')[0].addEventListener('webkitAnimationEnd', function () {
+                    $('.music-info .status-wrapper').removeClass('status-wrapper--anim');
+                });
             });
-        });
-        bindAudio(player);
-    }
-
-    function bindAudio(player) {
-        player.onplay = function () {
-            $('.music-info .J-status').addClass('fa-pause');
-            $('.music-info .J-status').removeClass('fa-play');
-            $('.music-info .J-status').removeClass('status--pause');
+            bindAudio(player);
         }
 
-        player.onpause = function () {
-            $('.music-info .J-status').removeClass('fa-pause');
-            $('.music-info .J-status').addClass('fa-play');
-            $('.music-info .J-status').addClass('status--pause');
-        }
-
-        player.onended = function () {
-            var index = Math.floor(Math.random() * 10);
-            changeSong(index);
-            $('.music-info .J-status').addClass('fa-pause');
-            $('.music-info .J-status').removeClass('fa-play');
-            $('.music-info .J-status').removeClass('status--pause');
-        }
-    }
-
-    function bindPlayList() {
-        $('.music-list-wrapper .J-trigger').hover(function () {
-            $(this).next('.music-list').show();
-            $(this).next('.music-list').addClass('music-list--expand');
-        });
-        $('.music-list').on('mouseleave, click', function () {
-            var that = this;
-            setTimeout(function () {
-                $(that).removeClass('music-list--expand');
-            }, 500);
-        });
-        $('.music-list li').hover(function () {
-            $('.active').removeClass('active');
-            $(this).addClass('active');
-        });
-        $('.music-list li').click(function () {
-            var index = $('.music-list li').index(this);
-            changeSong(index);
-        });
-    }
-
-    function changeSong(index) {
-        var ndTarget = $($('.music-list li')[index]),
-            id = ndTarget.find('.music__title').data('id'),
-            title = ndTarget.find('.music__title').data('title'),
-            url = ndTarget.find('.music__url').html();
-        $.ajax({
-            url: '/getMusicInfo?id=' + id,
-            method: 'get'
-        }).done(function(data) {
-            var music = JSON.parse(data),
-                ndInfo = $('.music-info'),
-                author = [];
-            
-            ndInfo.find('.cover')[0].src = music.image.replace(/spic/, 'lpic');
-            ndInfo.find('.name')[0].innerHTML = title;
-            ndInfo.find('.track')[0].innerHTML = music.alt_title;
-            for(var p in music.author) {
-                author.push(music.author[p].name); 
+        function bindAudio(player) {
+            player.onplay = function () {
+                $('.music-info .J-status').addClass('fa-pause');
+                $('.music-info .J-status').removeClass('fa-play');
+                $('.music-info .J-status').removeClass('status--pause');
             }
-            author = author.join('/');
-            ndInfo.find('.author')[0].innerHTML = author;
-            ndInfo.find('.description')[0].innerHTML = music.summary;
 
-            player.src = url;
-            setAudio($($('.music-list li')[index]).find('.music__url').html());
-        });
-    }
+            player.onpause = function () {
+                $('.music-info .J-status').removeClass('fa-pause');
+                $('.music-info .J-status').addClass('fa-play');
+                $('.music-info .J-status').addClass('status--pause');
+            }
 
-    // 设置音乐插件
-    function setAudio(url) {
-        $('.J-placeholder').html(ReferrerKiller.htmlString('<audio controls name="media"><source src="' + url + '" type="audio/mpeg"></audio>'));
-        player = $('.J-placeholder iframe')[0].contentWindow.document.querySelector('audio');
-        player.play();
-        bindAudio(player);
-    }
+            player.onended = function () {
+                var index = Math.floor(Math.random() * 10);
+                changeSong(index);
+                $('.music-info .J-status').addClass('fa-pause');
+                $('.music-info .J-status').removeClass('fa-play');
+                $('.music-info .J-status').removeClass('status--pause');
+            }
+        }
 
-    // 插入控制功能
-    function insertControllers() {
-        var innerWindow = $('.J-placeholder').find('iframe')[0].contentWindow,
-            ndBody = $(innerWindow.document.body);
-        ndBody.append('<script src="' + location.origin + '/javascripts/jquery-1.11.1.min.js"></script>');
-        player = ndBody[0].querySelector('audio');
-    }
+        function bindPlayList() {
+            $('.music-list-wrapper .J-trigger').hover(function () {
+                $(this).next('.music-list').show();
+                $(this).next('.music-list').addClass('music-list--expand');
+            });
+            $('.music-list').on('mouseleave, click', function () {
+                var that = this;
+                setTimeout(function () {
+                    $(that).removeClass('music-list--expand');
+                }, 500);
+            });
+            $('.music-list li').hover(function () {
+                $('.active').removeClass('active');
+                $(this).addClass('active');
+            });
+            $('.music-list li').click(function () {
+                var index = $('.music-list li').index(this);
+                changeSong(index);
+            });
+        }
+
+        function changeSong(index) {
+            var ndTarget = $($('.music-list li')[index]),
+                id = ndTarget.find('.music__title').data('id'),
+                title = ndTarget.find('.music__title').data('title'),
+                url = ndTarget.find('.music__url').html();
+            $.ajax({
+                url: '/getMusicInfo?id=' + id,
+                method: 'get'
+            }).done(function(data) {
+                var music = JSON.parse(data),
+                    ndInfo = $('.music-info'),
+                    author = [];
+                
+                ndInfo.find('.cover')[0].src = music.image.replace(/spic/, 'lpic');
+                ndInfo.find('.name')[0].innerHTML = title;
+                ndInfo.find('.track')[0].innerHTML = music.alt_title;
+                for(var p in music.author) {
+                    author.push(music.author[p].name); 
+                }
+                author = author.join('/');
+                ndInfo.find('.author')[0].innerHTML = author;
+                ndInfo.find('.description')[0].innerHTML = music.summary;
+
+                player.src = url;
+                setAudio($($('.music-list li')[index]).find('.music__url').html());
+            });
+        }
+
+        // 设置音乐插件
+        function setAudio(url) {
+            $('.J-placeholder').html(ReferrerKiller.htmlString('<audio controls name="media"><source src="' + url + '" type="audio/mpeg"></audio>'));
+            player = $('.J-placeholder iframe')[0].contentWindow.document.querySelector('audio');
+            player.play();
+            bindAudio(player);
+        }
+
+        // 插入控制功能
+        function insertControllers() {
+            var innerWindow = $('.J-placeholder').find('iframe')[0].contentWindow,
+                ndBody = $(innerWindow.document.body);
+            ndBody.append('<script src="' + location.origin + '/javascripts/jquery-1.11.1.min.js"></script>');
+            player = ndBody[0].querySelector('audio');
+        }
+
+    });
 });
