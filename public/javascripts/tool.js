3850a6c1721a5cc2572071681500e71672bafe19
linxixuan <zmx.morgan@gmail.com>
2014-12-01 23:43:47 +0800

    change style & use stylus & use requirejs

--- public/javascripts/tool.js
+++ public/javascripts/tool.js
@@ -1,6 +1,6 @@
 (function () {
     $('body').append('<i class="J-up-top up-top fa fa-chevron-up"></i>');
     $('.J-up-top').on('click', function () {
-        window.scrollTo(0, 0);
+        $('body').animate({scrollTop: 0}, '500');
     });
 })();
